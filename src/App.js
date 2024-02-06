f6dfb53ca4fc47e7fc817bb8b7b2929e5e0e2743
Desappan <cdesappan333@gmail.com>
2024-02-06 15:57:59 +0530

    one

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Api_call from './api_call';
+
+function App() {
+  return (
+    <div className="App">
+      <Api_call />
+    </div>
+  );
+}
+
+export default App;
